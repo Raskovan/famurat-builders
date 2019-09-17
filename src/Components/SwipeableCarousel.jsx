b6836c051fea0d5448d0535d481eed6f90aa253f
github username <github email address>
2019-09-17 07:43:20 -0400

    swipte component to func

--- src/Components/SwipeableCarousel.jsx
+++ src/Components/SwipeableCarousel.jsx
@@ -1,77 +1,70 @@
-import React, { Component } from 'react'
-
+import React, { useState, useEffect } from 'react'
 import '../styles/swipeable.css'
 
 const IMG_WIDTH = window.innerWidth
 const IMG_HEIGHT = window.innerHeight
+let wheelTimeout
+let transitionTimeout
+let lastTouch = 0
 
-class SwipeableCarousel extends Component {
-	wheelTimeout
-	transitionTimeout
-	lastTouch = 0
-	state = {
+function SwipeableCarousel(props) {
+	const [data, setData] = useState({
 		currentIndex: 0,
 		movement: 0,
 		transitionDuration: '0s'
-	}
-	componentWillUnmount = () => {
-		clearTimeout(this.transitionTimeout)
+	})
+
+	const { currentIndex, movement, transitionDuration } = data
+	const maxLength = props.imgs.length - 1
+	const maxMovement = maxLength * IMG_WIDTH
+
+	useEffect(() => {
+		return () => {
+			clearTimeout(transitionTimeout)
+		}
+	})
+
+	const handleTouchStart = e => {
+		lastTouch = e.nativeEvent.touches[0].clientX
 	}
 
-	handleWindowClick = e => {
-		console.log('click', e.target)
+	const handleTouchMove = e => {
+		const delta = lastTouch - e.nativeEvent.touches[0].clientX
+		lastTouch = e.nativeEvent.touches[0].clientX
+		handleMovement(delta)
 	}
 
-	handleTouchStart = e => {
-		this.lastTouch = e.nativeEvent.touches[0].clientX
+	const handleTouchEnd = () => {
+		handleMovementEnd()
+		lastTouch = 0
 	}
-	handleTouchMove = e => {
-		const delta = this.lastTouch - e.nativeEvent.touches[0].clientX
-		this.lastTouch = e.nativeEvent.touches[0].clientX
-		this.handleMovement(delta)
-	}
-	handleTouchEnd = () => {
-		this.handleMovementEnd()
-		this.lastTouch = 0
-	}
-	handleWheel = e => {
-		clearTimeout(this.wheelTimeout)
-		this.handleMovement(e.deltaX)
-		this.wheelTimeout = setTimeout(() => this.handleMovementEnd(), 100)
-	}
-	handleMovement = delta => {
-		clearTimeout(this.transitionTimeout)
 
-		this.setState(state => {
-			const maxLength = this.props.imgs.length - 1
+	const handleWheel = e => {
+		clearTimeout(wheelTimeout)
+		handleMovement(e.deltaX)
+		wheelTimeout = setTimeout(() => handleMovementEnd(), 100)
+	}
 
-			let nextMovement = state.movement + delta
-
+	const handleMovement = delta => {
+		clearTimeout(transitionTimeout)
+		setData(data => {
+			let nextMovement = data.movement + delta
 			if (nextMovement < 0) {
 				nextMovement = 0
 			}
-
 			if (nextMovement > maxLength * IMG_WIDTH) {
 				nextMovement = maxLength * IMG_WIDTH
 			}
-
-			return {
-				movement: nextMovement,
-				transitionDuration: '0s'
-			}
+			return { ...data, movement: nextMovement, transitionDuration: '0s' }
 		})
 	}
 
-	handleMovementEnd = () => {
-		const { movement, currentIndex } = this.state
-
+	const handleMovementEnd = () => {
 		const endPosition = movement / IMG_WIDTH
 		const endPartial = endPosition % 1
 		const endingIndex = endPosition - endPartial
 		const deltaInteger = endingIndex - currentIndex
-
 		let nextIndex = endingIndex
-
 		if (deltaInteger >= 0) {
 			if (endPartial >= 0.1) {
 				nextIndex += 1
@@ -82,88 +75,92 @@ class SwipeableCarousel extends Component {
 				nextIndex += 1
 			}
 		}
-
-		this.transitionTo(nextIndex, Math.min(0.5, 1 - Math.abs(endPartial)))
+		transitionTo(nextIndex, Math.min(0.5, 1 - Math.abs(endPartial)))
 	}
 
-	transitionTo = (index, duration) => {
-		this.setState({
+	const transitionTo = (index, duration) => {
+		setData({
+			...data,
 			currentIndex: index,
 			movement: index * IMG_WIDTH,
 			transitionDuration: `${duration}s`
 		})
+		transitionTimeout = setTimeout(() => {
+			setData({ ...data, transitionDuration: '0s' })
+		}, duration * 100)
+  }
+  
+  const handleClose = () => {
+    props.closeBtn()
+    setTimeout(() => {
+      setData({
+        ...data,
+        currentIndex: 0,
+        movement: 0,
+        transitionDuration: '0s'
+      })
+		}, 1000)
+  }
 
-		this.transitionTimeout = setTimeout(() => {
-			this.setState({ transitionDuration: '0s' })
-		}, duration * 1000)
-	}
-	render() {
-		const { currentIndex, movement, transitionDuration } = this.state
-		const maxLength = this.props.imgs.length - 1
-		const maxMovement = maxLength * IMG_WIDTH
-
-		return (
-			<>
-				<div className='close-button'>
-					<img
-						className='close-image'
-						src={require(`../assets/close.svg`)}
-						alt='Closw Button'
-						onClick={this.props.closeBtn}
-					/>
-				</div>
+	return (
+		<>
+			<div className='close-button'>
+				<img
+					className='close-image'
+					src={require(`../assets/close.svg`)}
+					alt='Closw Button'
+					onClick={handleClose}
+				/>
+			</div>
+			<div
+				className='main'
+				style={{
+					width: `${IMG_WIDTH}px`,
+					height: `${IMG_HEIGHT}px`
+				}}
+				onTouchStart={handleTouchStart}
+				onTouchMove={handleTouchMove}
+				onTouchEnd={handleTouchEnd}
+				onWheel={handleWheel}>
 				<div
-					className='main'
+					className='swiper'
 					style={{
-						width: `${IMG_WIDTH}px`,
-						height: `${IMG_HEIGHT}px`
-					}}
-					onTouchStart={this.handleTouchStart}
-					onTouchMove={this.handleTouchMove}
-					onTouchEnd={this.handleTouchEnd}
-					onWheel={this.handleWheel}>
-					<div
-						className='swiper'
-						style={{
-							transform: `translateX(${movement * -1}px)`,
-							transitionDuration: transitionDuration
-						}}>
-						{this.props.imgs.map(src => (
-							<img
-								key={src.image_url}
-								src={src.image_url}
-								alt={src.title}
-								width='100%'
-								height={
-									src.width < src.height
-										? '100%'
-										: IMG_WIDTH / (src.width / src.height) + 'px'
-								}
-							/>
-						))}
-					</div>
-					{movement !== 0 && (
-						<div
-							className='back move'
-							onClick={() => {
-								this.transitionTo(currentIndex - 1, 0.5)
-							}}>
-							{'<'}
-						</div>
-					)}
-					{movement !== maxMovement && (
-						<div
-							className='next move'
-							onClick={() => {
-								this.transitionTo(currentIndex + 1, 0.5)
-							}}>
-							>
-						</div>
-					)}
+						transform: `translateX(${movement * -1}px)`,
+						transitionDuration: transitionDuration
+					}}>
+					{props.imgs.map(src => (
+						<img
+							key={src.image_url}
+							src={src.image_url}
+							alt={src.title}
+							width='100%'
+							height={
+								src.width < src.height
+									? '100%'
+									: IMG_WIDTH / (src.width / src.height) + 'px'
+							}
+						/>
+					))}
 				</div>
-			</>
-		)
-	}
+				{movement !== 0 && (
+					<div
+						className='back move'
+						onClick={() => {
+							transitionTo(currentIndex - 1, 0.5)
+						}}>
+						{'<'}
+					</div>
+				)}
+				{movement !== maxMovement && (
+					<div
+						className='next move'
+						onClick={() => transitionTo(currentIndex + 1, 0.5)}>
+						>
+					</div>
+				)}
+			</div>
+		</>
+	)
 }
 
 export default SwipeableCarousel
